00f3ed8d680c4d665afa2c868deed16aab49b7e2
Erdal ALTIN <erdalaltin@gmail.com>
2023-10-20 10:07:34 +0300

    Update IyzicoConfig.php

--- src/Http/Controllers/IyzicoConfig.php
+++ src/Http/Controllers/IyzicoConfig.php
@@ -35,6 +35,6 @@ class IyzicoConfig
             return "https://sandbox-api.iyzipay.com";
         }
 
-		return "https://api.merchant.iyzico.com";
+        return "https://api.merchant.iyzico.com";
     }
 }
