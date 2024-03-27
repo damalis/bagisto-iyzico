e364570437b3e8659d7a68b49d7e6102ac5f98a0
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-27 15:28:51 +0300

    Create app.php

---
+++ src/Resources/lang/tr/app.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'admin' => [
+        'system' => [
+            'iyzicoPayment' => 'Iyzico Payment',
+            'title' => 'Başlık',
+            'description' => 'Açıklama',
+            'status' => 'Durum',
+			'sandbox' => 'Test Durumu',
+            'public-key' => 'Public Key',
+            'secret-key' => 'Secret Key',
+			//'allowed-installments' => 'Taksit Sayıları',
+            'sort' => 'Sıralama Sırası',
+			'info' => 'İnternetten alışveriş deneyimini hem alıcılar hem de satıcılar için kolaylaştıran bir finansal teknolojiler şirketidir.',
+        ],
+
+    ],'shop' => [
+        'went-wrong' => 'Something went wrong !',
+        'payment-cancelled' => 'Iyzico Payment has been cancelled',
+    ]
+];  
