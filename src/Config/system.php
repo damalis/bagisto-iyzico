6e429b53052ebda8b1399cf6cae1aa7c6ce68d1c
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-27 15:28:12 +0300

    Update system.php

--- src/Config/system.php
+++ src/Config/system.php
@@ -2,39 +2,43 @@
 
 return [
     [
-        'key' => 'sales.paymentmethods.iyzico',
+        'key' => 'sales.payment_methods.iyzico',
         'name' => 'Iyzico',
+		'info' => 'iyzico::app.admin.system.info',
+		//'hot_file' => 'iyzico.png',
+		//'build_directory' => 'themes/blog/build',
+		//'package_assets_directory' => 'src/Resources/assets',
         'sort' => 5,
         'fields' => [
             [
                 'name' => 'title',
-                'title' => 'admin::app.admin.system.title',
+                'title' => 'iyzico::app.admin.system.title',
                 'type' => 'text',
                 'validation' => 'required',
                 'channel_based' => false,
                 'locale_based' => true
-            ],[
+            ], [
                 'name' => 'description',
-                'title' => 'admin::app.admin.system.description',
+                'title' => 'iyzico::app.admin.system.description',
                 'type' => 'textarea',
                 'validation' => 'required',
                 'channel_based' => false,
                 'locale_based' => true
-            ],[
+            ], [
                 'name' => 'public_key',
                 'title' => 'iyzico::app.admin.system.public-key',
                 'type' => 'text',
                 'validation' => 'required',
                 'channel_based' => false,
                 'locale_based' => true
-            ],[
+            ], [
                 'name'          => 'secret_key',
                 'title'         => 'iyzico::app.admin.system.secret-key',
                 'type'          => 'text',
                 'validation'    => 'required',
                 'channel_based' => false,
                 'locale_based'  => true,
-            ],[
+            ],/* [
                 'name'          => 'allowed_installments',
                 'title'         => 'iyzico::app.admin.system.allowed-installments',
                 'type'			=> 'text',
@@ -42,20 +46,45 @@ return [
 				'default_value' => '0',
                 'channel_based' => false,
                 'locale_based'  => true,
-            ],[
+				'default_value' => '1',
+            ], */[
                 'name' => 'sandbox',
-                'title' => 'admin::app.admin.system.sandbox',
+                'title' => 'iyzico::app.admin.system.sandbox',
                 'type' => 'boolean',
                 'channel_based' => false,
                 'locale_based'  => true
-            ],[
-                'name' => 'active',
-                'title' => 'admin::app.admin.system.status',
+            ], [
+                'name' => 'status',
+                'title' => 'iyzico::app.admin.system.status',
                 'type' => 'boolean',                
                 'validation' => 'required',
                 'channel_based' => false,
                 'locale_based'  => true
-            ]     
+            ], [
+                'name' => 'sort',
+                'title' => 'iyzico::app.admin.system.sort',
+                'type' => 'select',
+                'channel_based' => false,
+                'locale_based'  => true,
+				'options' => [
+                    [
+                        'title' => '1',
+                        'value' => '1',
+                    ], [
+                        'title' => '2',
+                        'value' => '2',
+                    ], [
+                        'title' => '3',
+                        'value' => '3',
+                    ], [
+                        'title' => '4',
+                        'value' => '4',
+                    ], [
+                        'title' => '5',
+                        'value' => '5',
+                    ],
+                ],
+            ],			
         ],
     ],
    
