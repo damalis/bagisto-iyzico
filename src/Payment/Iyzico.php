4bc683c4ed0fc851fb3ae012647f23ee8ab16771
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-28 10:40:48 +0300

    Update Iyzico.php

--- src/Payment/Iyzico.php
+++ src/Payment/Iyzico.php
@@ -22,4 +22,17 @@ class Iyzico extends Payment
     {
         return route('iyzico.payment.checkout');        
     }
+	
+	/**
+     * Returns payment method image
+     *
+     * @return array
+     *//*
+    public function getImage()
+    {
+        $url = $this->getConfigData('image');
+
+        return $url ? Storage::url($url) : bagisto_asset('images/iyzico.png', 'shop');
+
+    }*/
 }
