47f3e0797c003218fb257ff1d6a0fb6f7c67e996
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-29 11:57:58 +0300

    Update paymentmethods.php

--- src/Config/paymentmethods.php
+++ src/Config/paymentmethods.php
@@ -5,6 +5,7 @@ return [
         'code' => 'iyzico',
         'title' => 'Iyzico',
         'description' => 'Iyzico Payment Gateway',
+        'image' => asset('/vendor/damalis/iyzico/assets/images/iyzico.png'),
         'class' => 'Damalis\Iyzico\Payment\Iyzico',
         'active' => true,
         'sort'=> 5,
