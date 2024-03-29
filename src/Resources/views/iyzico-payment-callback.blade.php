96a77862b02520b5e25fa5e57272a99f3a343e01
Erdal ALTIN <erdalaltin@gmail.com>
2024-03-29 11:56:10 +0300

    Update iyzico-payment-callback.blade.php

--- src/Resources/views/iyzico-payment-callback.blade.php
+++ src/Resources/views/iyzico-payment-callback.blade.php
@@ -1,9 +1,18 @@
-@extends('shop::layouts.master')
+<x-shop::layouts.account>
+    <!-- Title of the page -->
+    <x-slot:title>
+        @lang('blog::app.shop.blogs.page-title')
+    </x-slot:title>
 
-@section('page_title')
-    {{ __('Iyzico Checkout....') }}
-@stop
+    <div class="flex justify-between items-center">
+        <h2 class="text-[26px] font-medium">
+            @section('page_title')
+				{{ __('Iyzico Checkout....') }}
+			@stop
 
-@push('scripts')
-    <?php echo $paymentcontent_msg; ?>
-@endpush
+			@push('scripts')
+				<?php echo $paymentcontent_msg; ?>
+			@endpush
+        </h2>
+    </div>
+</x-shop::layouts.account>
