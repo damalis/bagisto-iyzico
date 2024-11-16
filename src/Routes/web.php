f60b59071cf4c0be4bea88e4d3c4b0cf2d3d4d51
damalis <erdalaltin@gmail.com>
2024-11-16 11:47:39 +0300

    Update web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Middleware\VerifyCsrfToken;
 
 use Damalis\Iyzico\Http\Controllers\IyzicoController;
 
-Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function () {    
+Route::group(['middleware' => ['web', 'theme', 'locale', 'currency']], function () {
     Route::get('iyzico-payment-checkout', [
         IyzicoController::class, 'checkoutWithIyzico'])->name('iyzico.payment.checkout');    
     Route::post('iyzico-payment-callback/{token}', [
